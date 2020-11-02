ecf2c32db804a2db95fd0e9a65043e915b31b0ed
solo <iamsolo09@gmail.com>
2020-11-02 12:51:50 +0530

    Audio Added on Thrusting
    Whenever the user presses space key to thrust the rocket, an audio of rocket thrusting will be played. When the user stops pressing the space key, the audio will also be stopped.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
+    Rigidbody rigidBodyRocket;
+    AudioSource rocketThrustAudio;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent <Rigidbody>();
+        rigidBodyRocket = GetComponent <Rigidbody>();
+        rocketThrustAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,7 +27,14 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space)) 
         {
-            rigidBody.AddRelativeForce(Vector3.up); 
+            rigidBodyRocket.AddRelativeForce(Vector3.up);
+            if(!rocketThrustAudio.isPlaying)
+            {
+                rocketThrustAudio.Play();
+            }
+        } else
+        {
+            rocketThrustAudio.Stop();
         }
 
         if(Input.GetKey(KeyCode.A))
@@ -35,5 +44,7 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        
     }
  }
